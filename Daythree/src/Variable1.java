b930c60c6cbcb25be420b5d981e4e37756920d9e
srinivas s g <srinivas s g@DESKTOP-3PFA54V>
2023-08-10 12:09:28 +0530

    first commit

---
+++ Daythree/src/Variable1.java
@@ -0,0 +1,15 @@
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short myNum = 5000;
+		 myNum=10000;
+		/*jsvsagdjgjkggghhhhhhhhhhhhhhhhhhh
+		  dyyyyyyyyyyyyyyyyy*/
+		
+		System.out.println(myNum);
+
+	}
+
+}
