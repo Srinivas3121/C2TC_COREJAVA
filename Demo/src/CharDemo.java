2ee5d1dbfe7e8880e2fbe1b7fb25a8bc6ce1d82e
srinivas s g <srinivas s g@DESKTOP-3PFA54V>
2023-08-10 12:03:15 +0530

    first commit

---
+++ Demo/src/CharDemo.java
@@ -0,0 +1,15 @@
+import javax.annotation.processing.SupportedSourceVersion;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='a';
+		System.out.println(ch);
+		char chone = 65;
+		System.out.println(chone);
+		
+
+	}
+
+}
